1e3a55d8fc6920a6583e4ae53cd6137e3179bd67
Nikita_Kharitonov <118862999+Nikita-Kharitonov@users.noreply.github.com>
2022-12-02 21:47:09 +0700

    comment

--- Users/Петровна/Desktop/GB_home/C#_dz2/Program.cs
+++ Users/Петровна/Desktop/GB_home/C#_dz2/Program.cs
@@ -1,11 +1,11 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
 //показывает вторую цифру этого числа. Через строку решать нельзя.
 
-//Console.WriteLine("Введите трехзначное число");
-//int num = Convert.ToInt32(Console.ReadLine());
-//if (num < 1000 && num > 99)
-//    Console.WriteLine($"Вторая цифра {num / 10 % 10}");
-//else Console.WriteLine("Введенно неверное число");
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1000 && num > 99)
+    Console.WriteLine($"Вторая цифра {num / 10 % 10}");
+else Console.WriteLine("Введенно неверное число");
 
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
@@ -27,8 +27,8 @@
 //Задача 15: Напишите программу, которая принимает на вход цифру, 
 //обозначающую день недели, и проверяет, является ли этот день выходным.
 
- Console.WriteLine("Введите номер дня");
- int day = Convert.ToInt32(Console.ReadLine());
- if (day > 0 && day < 6) Console.WriteLine("Выбраный день рабочий");
- else if (day > 5 && day < 8) Console.WriteLine("Выбранный день выходной");
- else Console.WriteLine("Номер дня введен неверно");
+// Console.WriteLine("Введите номер дня");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day > 0 && day < 6) Console.WriteLine("Выбраный день рабочий");
+// else if (day > 5 && day < 8) Console.WriteLine("Выбранный день выходной");
+// else Console.WriteLine("Номер дня введен неверно");
